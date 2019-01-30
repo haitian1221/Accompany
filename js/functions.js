cf288386a73d9adf0abb3f9a372119e51316336c
haitian1221 <729221707@qq.com>
2019-01-30 09:46:05 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -93,7 +93,7 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = new Date();
+	var current = Date();
 	var seconds = (Date.parse(current) - Data.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
